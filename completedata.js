ab6103f35f261708aef6356dd06d5e9bdcf11e0b
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-05 15:19:21 -0600

    leetcode practice

---
+++ completedata.js
@@ -0,0 +1,54 @@
+// Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+
+// For example, given this array of posts (note that the submitted_by is an id number):
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+// ]
+
+// And this array of users:
+
+// [
+// {user_id: 403, name: "Aunty Em"},
+// {user_id: 231, name: "Joelle P."},
+// {user_id: 989, name: "Lyndon Johnson"},
+// {user_id: 111, name: "Patti Q."},
+// ]
+
+// Return the array of posts as follows:
+
+// [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+var input = [
+  {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+]
+
+var users = [
+{user_id: 403, name: "Aunty Em"},
+{user_id: 231, name: "Joelle P."},
+{user_id: 989, name: "Lyndon Johnson"},
+{user_id: 111, name: "Patti Q."},
+]
+  
+const update = (input, users) => { 
+  return input.map((post) => { 
+    const user = users.find(user => user.user_id === post.submitted_by)
+    return { 
+     ...post,
+     submitted_by: user ? user.name : 'Unknown'
+    }
+  })
+}
+
+console.log(update(input, users))
