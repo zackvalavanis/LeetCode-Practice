4cbbc67355de5600143355bda7d9d8d9675d6b69
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-04 20:11:15 -0600

    leetcode practice

---
+++ orderWholeMenu.js
@@ -0,0 +1,20 @@
+// Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+// Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+// Output: 14
+
+// Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+var input = {"hot dog": 2, "hamburger": 3, "steak sandwich": 5, "fries": 1, "cole slaw": 1, "soda" : 2}
+
+const costOfAll = (input) => { 
+  var sum = 0;
+  for(const key in input){ 
+    var values = input[key]
+    sum += values
+  }
+return sum
+}
+
+console.log(costOfAll(input))
