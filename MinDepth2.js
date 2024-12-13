a9902d3eae5c95ab68640a1207f7158577639472
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-13 10:33:01 -0600

    leetcode practice

---
+++ MinDepth2.js
@@ -0,0 +1,26 @@
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+root = new TreeNode(3)
+root.left = new TreeNode(9)
+root.right = new TreeNode(20)
+root.right.right = new TreeNode(7)
+root.right.left = new TreeNode(15)
+
+const minDepth = (root) => { 
+  const height = (node) => { 
+    if(!node) return 0
+    if(!node.left && !node.right) return 1;
+
+    if(!node.left) return height(node.right) + 1
+    if(!node.right) return height(node.left) + 1
+
+    return Math.min(height(node.left), height(node.right)) + 1
+  }
+  return height(root)
+}
+
+console.log(minDepth(root))
