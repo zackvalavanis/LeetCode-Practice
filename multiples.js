6417f4fac34407d5bb2db69c79acc5c76190bd30
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-14 12:07:40 -0600

    leetcode practice

---
+++ multiples.js
@@ -0,0 +1,18 @@
+// Description
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+var n = 1000
+
+const sumMultiples = (n) => { 
+  var sum = 0;
+  for(let i = 0; i < n; i++){ 
+    if(i % 3 === 0 || i % 5 === 0){ 
+      sum += i
+    } 
+  }
+  return sum
+}
+
+console.log(sumMultiples(n))
