ca80e6e126de298bad335c5c5a7e52f8edd3ee35
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-02 11:30:29 -0600

    leetcode practice

--- January-2-2025/MergeTwoSortedLists.js
+++ January-2-2025/MergeTwoSortedLists.js
@@ -4,30 +4,50 @@
 
 // Return the head of the merged linked list.
 
-function ListNode(val, next) { 
-  this.val = (val===undefined ? 0 : val), 
-  this.next = (next===undefined ? null: next)
-}
 
-const node = new ListNode(1)
-node.next = new ListNode(2)
-node.next.next = new ListNode(4)
-
-const node2 = new ListNode(2)
-node2.next = new ListNode(3)
-node2.next.next = new ListNode(5)
-
-
-const mergeLists = (node, node2) => { 
-  if(!node) return node2
-  if(!node2) return node
-  if(node.val < node2.val){ 
-    node.next = mergeLists(node.next, node2)
-    return node
-  } else { 
-    node2.next = mergeLists(node, node2.next)
-    return node2
+//recursive 
+// const mergeLists = (node, node2) => { 
+  //   if(!node) return node2
+  //   if(!node2) return node
+  //   if(node.val < node2.val){ 
+    //     node.next = mergeLists(node.next, node2)
+    //     return node
+    //   } else { 
+      //     node2.next = mergeLists(node, node2.next)
+      //     return node2
+      //   }
+      // }
+  
+  function ListNode(val, next) { 
+    this.val = (val===undefined ? 0 : val), 
+    this.next = (next===undefined ? null: next)
   }
+  
+  const node = new ListNode(1)
+  node.next = new ListNode(2)
+  node.next.next = new ListNode(4)
+  
+  const node2 = new ListNode(2)
+  node2.next = new ListNode(3)
+  node2.next.next = new ListNode(5)
+    
+
+function merge (node, node2) { 
+  let dummy = new ListNode(-1)
+  let current = dummy
+
+  while(node && node2){ 
+    if(node.val <= node2.val){ 
+      current.next = node
+      node = node.next
+    } else { 
+      current.next = node2
+      node2 = node2.next
+    }
+    current = current.next
+  }
+  current.next = node || node2
+  return dummy.next
 }
 
 // Utility Function 
@@ -41,6 +61,6 @@ function printList (head) {
   return result.join(' -> ')
 }
 
-const mergeList = (mergeLists(node, node2))
+const mergeList = merge(node, node2);
 
 console.log(printList(mergeList))
