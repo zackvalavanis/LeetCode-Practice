276cca82df2713eca5776df21b1b24b7e6b99a67
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-28 19:11:25 -0600

    leetcode practice

---
+++ January-28-2025/squaresorted.js
@@ -0,0 +1,22 @@
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+
+var nums =  [-4,-1,0,3,10]
+
+const sorted = (nums) => { 
+  for(let i = 0; i < nums.length; i++){
+    nums[i] = nums[i] * nums[i]
+  }
+  for(let i = 0; i < nums.length; i++){ 
+    for(let j = i + 1; j < nums.length; j++) { 
+      if(nums[i] > nums[j]){ 
+        var temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+      }
+    }
+  }
+  return nums
+}
+
+console.log(sorted(nums))
