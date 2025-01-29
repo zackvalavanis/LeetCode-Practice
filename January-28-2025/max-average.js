cda4d6af01b6bbe29edf4260d53ebbc36b1c03d8
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-28 18:49:38 -0600

    leetcode practice

---
+++ January-28-2025/max-average.js
@@ -0,0 +1,18 @@
+var nums = [1,12,-5,-6,50,3]
+var k = 4
+
+const maxAverage = (nums, k) => { 
+  var currentSum = 0;
+
+  for(let i = 0; i < k; i++){ 
+    currentSum += nums[i]
+  }
+  var maxSum = currentSum
+  for(let i = k; i < nums.length; i++){ 
+    currentSum = nums[i] - nums[i-k]
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum / k
+}
+
+console.log(maxAverage(nums, k))
