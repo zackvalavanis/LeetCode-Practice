9f69753fab3a31af642d25ffa73b84148636a0f2
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-13 20:24:26 -0600

    leetcode practice

---
+++ February-13-2025/etl3.js
@@ -0,0 +1,55 @@
+// Given an array of Youtube videos, for example:
+
+// [
+// {title: 'How to Make Wood', author_id: 4, views: 6},
+// {title: 'How to Seem Perfect', author_id: 4, views: 111},
+// {title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202},
+// {title: 'Why Pigs Stink', author_id: 1, views: 12}
+// ]
+
+// and an array of authors, for example:
+
+// [
+// {id: 1, first_name: 'Jazz', last_name: 'Callahan'},
+// {id: 2, first_name: 'Ichabod', last_name: 'Loadbearer'},
+// {id: 3, first_name: 'Saron', last_name: 'Kim'},
+// {id: 4, first_name: 'Teena', last_name: 'Burgess'},
+// ]
+
+// Return a new array of videos in the following format, and only include videos that have at least 100 views:
+
+// [
+// {title: 'How to Seem Perfect', views: 111, author_name: 'Teena Burgess' }
+// {title: 'Review of the New "Unbreakable Mug"', views: 202, author_name: 'Ichabod Loadbearer' },
+// ]
+
+var input = 
+[
+{title: 'How to Make Wood', author_id: 4, views: 6},
+{title: 'How to Seem Perfect', author_id: 4, views: 111},
+{title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202},
+{title: 'Why Pigs Stink', author_id: 1, views: 12}
+]
+
+var author = 
+[
+{id: 1, first_name: 'Jazz', last_name: 'Callahan'},
+{id: 2, first_name: 'Ichabod', last_name: 'Loadbearer'},
+{id: 3, first_name: 'Saron', last_name: 'Kim'},
+{id: 4, first_name: 'Teena', last_name: 'Burgess'},
+]
+
+const combine = (input, author) => { 
+  return input.map((info) => { 
+    var foundName = author.find(a => a.id === info.author_id)
+    return [ { 
+      title: info.title, 
+      views: info.views, 
+      author_name: foundName.first_name + ' ' + foundName.last_name
+    }
+    ]
+  })
+}
+
+console.log(combine(input, author))
+
