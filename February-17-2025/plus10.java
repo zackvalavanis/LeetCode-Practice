3517523fa33bb32aeb19ee680790684be86aee51
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-18 13:40:59 -0600

    delierate practice - first Java files

---
+++ February-17-2025/plus10.java
@@ -0,0 +1,8 @@
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+public class plus10 {
+  public static void main(String[] args) {
+    int number = 5;
+    System.out.println(number + 10);
+  }
+}
