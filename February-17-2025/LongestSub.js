0fea8268cc3c6901ccdb5a2e0369190787297998
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-18 12:25:16 -0600

    leetcode practice

---
+++ February-17-2025/LongestSub.js
@@ -0,0 +1,23 @@
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+
+var s = "abcabcbb"
+
+const longestSub = (s) => {
+  var set = new Set()
+  var left = 0
+  var maxSum = 0;
+  for(let right = 0; right < s.length; right++){ //iterate through
+    while(set.has(s[right])){ 
+      set.delete(s[left])
+      left++
+    }
+    set.add(s[right])
+    maxSum = Math.max(maxSum, right - left + 1)
+  }
+  return maxSum
+}
+
+console.log(longestSub(s))
+
