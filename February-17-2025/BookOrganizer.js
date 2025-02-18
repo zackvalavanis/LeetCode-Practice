a71193f1fc9361d49631701109e3f39db99a4406
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-18 11:57:44 -0600

    leetcode practice

---
+++ February-17-2025/BookOrganizer.js
@@ -0,0 +1,58 @@
+// Given a list of books provided in this format:
+
+// [
+// {title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+// {title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+// {title: "1984", author: "George Orwell", year: 1949 },
+// {title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+// {title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+// {title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+// {title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+// ]
+
+// return the data in this new author-centric format:
+
+// {     "J. R. R. Tolkien" => [
+// {title: "The Lord of the Rings", year: 1954 },
+// {title: "The Hobbit", year: 1937 },
+// {title: "The Two Towers", year: 1954 }
+// ],
+// "Harper Lee" => [
+// {title: "To Kill a Mockingbird", year: 1960 },
+// {title: "Go Set a Watchman", year: 2015 }
+// ],
+// "George Orwell" => [
+// {title: "1984", year: 1949 }
+// ],
+// "F. Scott Fitzgerald" => [
+// {title: "The Great Gatsby", year: 1925 }
+// ]
+// }
+
+var input = 
+[
+{title: "The Lord of the Rings", author: "J. R. R. Tolkien", year: 1954 },
+{title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+{title: "1984", author: "George Orwell", year: 1949 },
+{title: "Go Set a Watchman", author: "Harper Lee", year: 2015 },
+{title: "The Hobbit", author: "J. R. R. Tolkien", year: 1937 },
+{title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+{title: "The Two Towers", author: "J. R. R. Tolkien", year: 1954 }
+]
+
+
+const combined = (input) => { 
+  return input.reduce((acc, book) => { 
+    if(!acc[book.author]) { 
+      acc[book.author] = [];
+    }
+
+    acc[book.author].push ({
+      title: book.title, 
+      year: book.year
+    });
+    return acc
+  }, {})
+}
+
+console.log(combined(input))
