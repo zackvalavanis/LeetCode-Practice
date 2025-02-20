ef23834bc23fd435ffb0e9a842f4b659c72e661b
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-20 13:52:24 -0600

    Ruby and Java Practice

---
+++ February-20-2025/AddNumbers.java
@@ -0,0 +1,8 @@
+public class AddNumbers {
+  public static void main(String[] arg) {
+    String word1 = "Hello";
+    String word2 = "Zack";
+
+    System.out.println(word1 + " " + word2);
+  }
+}
---
+++ February-20-2025/AddNumbers.rb
@@ -0,0 +1,4 @@
+word1 = 'hi'
+word2 = 'zack'
+
+pp word1 + ' ' + word2
