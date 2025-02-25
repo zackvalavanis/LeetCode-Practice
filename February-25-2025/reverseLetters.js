511ac67d012b0324cf03c01bb841dc8696b4198a
Zack <168221270+Zval321@users.noreply.github.com>
2025-02-25 12:17:52 -0600

    leetcode practice

---
+++ February-25-2025/reverseLetters.js
@@ -0,0 +1,30 @@
+// Given a string s, reverse the string according to the following rules:
+
+// All the characters that are not English letters remain in the same position.
+// All the English letters (lowercase or uppercase) should be reversed.
+// Return s after reversing it.
+
+var s = "ab-cd"
+// Output: "dc-ba"
+
+const reverse = (s) => { 
+  var arr = s.split('')
+  var left = 0; 
+  var right = arr.length - 1
+  while(left < right){ 
+    if(!/[a-zA-z]/.test(arr[left])) { 
+      left++
+    } else if(!/[a-zA-z]/.test(arr[right])){ 
+      right--
+    } else { 
+      var temp = arr[left]
+      arr[left] = arr[right]
+      arr[right] = temp
+      left++
+      right--
+    }
+  }
+  return arr.join('')
+}
+
+console.log(reverse(s))
