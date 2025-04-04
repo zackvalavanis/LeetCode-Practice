903d1151bbf5a5a5f8ccf0d80d49ce8e1316e69a
Zack <168221270+Zval321@users.noreply.github.com>
2025-04-04 12:20:14 -0500

    leetcode practice

---
+++ April-4-2025/maxSum.js
@@ -0,0 +1,21 @@
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+var nums = [1,12,-5,-6,50,3]
+var k = 4
+
+const maxAverage = (nums, k) => { 
+  var currentSum = 0;
+  for(let i = 0; i < k; i++){ 
+    currentSum += nums[i]
+  }
+  let maxSum = currentSum
+  for(let i = k; i < nums.length; i++){ 
+    currentSum += nums[i] - nums[i-k] 
+    maxSum = Math.max(maxSum, currentSum)
+  }
+  return maxSum / k
+}
+
+console.log(maxAverage(nums, k))
